02a25d4e0b7ad445b62b804b5ebd590ccc3840e5
Shahrukh <shahrukh1494@gmail.com>
2024-09-27 12:18:51 +0530

    first commit

---
+++ src/components/TextComponent.jsx
@@ -0,0 +1,9 @@
+const TextComponent = () => {
+  return (
+    <div className="hello-world-container">
+      <span className="hello-world">Hello World!</span>
+    </div>
+  );
+};
+
+export default TextComponent;
